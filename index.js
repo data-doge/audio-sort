78f647fb466344701ac552abd950118d416a3106
Eugene Lynch <eugenestapler@gmail.com>
2018-08-17 01:57:40 -0400

    too drunk to know if working

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const { exec } = require('child_process')
+const { readdir, readFile } = require('fs')
+const decode = require('audio-decode')
+const average = require('average')
+const map = require('async/map')
+const sortBy = require('lodash.sortby')
+const audioconcat = require('audioconcat')
+const compact = require('lodash.compact')
 const inputFile = process.argv[2]
-const sampleLength = 0.5
+const sampleLength = parseFloat(process.argv[3]) || 0.1
 
-exec(`mkdir -p tmp`, err => {
+exec('rm -rf tmp', err => {
   if (err) throw err
-  exec(`ffmpeg -i ${inputFile} -f segment -segment_time ${sampleLength} -c copy tmp/clip%03d.mp3`, err => {
+  exec(`mkdir tmp`, err => {
     if (err) throw err
+    exec(`ffmpeg -i ${inputFile} -f segment -segment_time ${sampleLength} -c copy tmp/clip%03d.mp3`, err => {
+      if (err) throw err
+
+      readdir('tmp', (err, clips) => {
+        if (err) throw err
+        const getRms = (clip, next) => {
+          const filePath = `tmp/${clip}`
+          readFile(filePath, (err, buf) => {
+            if (err) throw err
+            decode(buf).then(audioBuf => {
+              const rms = Math.sqrt(average(audioBuf.getChannelData(0).map(d => d * d)))
+              next(null, { filePath, rms })
+            }).catch(err => {
+              console.error(err)
+              next(null)
+            })
+          })
+        }
+
+        map(clips, getRms, (err, clipData) => {
+          if (err) throw err
+          const filesSortedByAmp = compact(sortBy(clipData, 'rms')).map(d => d.filePath)
+          audioconcat(filesSortedByAmp).concat('output.mp3')
+        })
+      })
+    })
   })
 })
